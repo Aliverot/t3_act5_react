732fab6f18aa779ef29917d76f6d06eaa09442e8
Aliverot <mcaliverot@gmail.com>
2026-07-10 01:14:55 -0600

    Configuración de scripts y base para despliegue en GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/t3_act5_react/', // <-- Esta es la línea que debes agregar
 })
